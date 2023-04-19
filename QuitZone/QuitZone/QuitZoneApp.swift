e8b149e47a2c8ff209144f0784342d58a76584ef
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2023-04-19 13:06:58 +0700

    Init Project

---
+++ QuitZone/QuitZone/QuitZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuitZoneApp.swift
+//  QuitZone
+//
+//  Created by Leonardo Wijaya on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct QuitZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
