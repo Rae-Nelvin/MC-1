093cd37004ea4fd8e1960335469670da19778789
ndyy <handy.darmawan@binus.ac.id>
2023-04-22 01:01:37 +0700

    Progress in home page & mission page

--- QuitZone/QuitZone/Features/ViewModel/Components/MissionComponent.swift
+++ QuitZone/QuitZone/Features/ViewModel/Components/MissionComponent.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct MissionComponent: View {
+    
+    let columns = Array(repeating: GridItem(), count: 5)
+    let datas = 1...30
+    
     var body: some View {
-        Text("INI MISSION")
+        VStack {
+            Text("**Daily Mission**")
+                .customText(size: 36)
+                .hAlign(.center)
+                .padding(.bottom, 30)
+            
+            LazyVGrid(columns: columns) {
+                 ForEach(datas, id: \.self) {data in
+                    VStack {
+                        Text(String(data))
+                    }
+                    .frame(width: 40, height: 40)
+                    .background(.red)
+                    .padding(1)
+                    
+                }
+            }
+            .hAlign(.center)
+            
+        }
+        .vAlign(.top)
+        .padding()
     }
 }
 
