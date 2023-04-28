f60e07e12f4adb7fc8306b918380213d364f54b0
Jonathan Evan <jonathanevanc@gmail.com>
2023-04-28 16:32:25 +0700

    Main Team View

---
+++ QuitZone/QuitZone/Features/Team/View/Views/MainTeamView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTeamView.swift
+//  QuitZone
+//
+//  Created by Jonathan Evan Christian on 28/04/23.
+//
+
+import SwiftUI
+
+struct MainTeamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainTeamView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTeamView()
+    }
+}
